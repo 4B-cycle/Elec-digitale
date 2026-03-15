c7815a35371942d7c19ae257966c8647dd4f8ef6
Dmytro Svitkin <77505900+Dmytro-Svitkin@users.noreply.github.com>
2026-03-15 02:06:39 +0100

    changes mineurs

--- script.js
+++ script.js
@@ -4,11 +4,28 @@ let data=["{l:}<span class=empty>:::::::::::::::::::::::::</span>",
           "{l:}<span class=empty>:::::::::::::::::::::::::</span>",
           "{l:}<span class=empty>:::::::::::::::::::::::::</span>"];
 
-function Start() {
-    /*
+let vitesse=1;
+
+function WriteData(a, b, c, d) {
+    data[0] = data[0].replace("}", a+",}");data[0] = data[0].replace(":<", "<");
+    data[1] = data[1].replace("}", b+",}");data[1] = data[1].replace(":<", "<");
+    data[2] = data[2].replace("}", c+",}");data[2] = data[2].replace(":<", "<");
+    data[3] = data[3].replace("}", d+",}");data[3] = data[3].replace(":<", "<");
+    /*for (let datum of data) {
+            datum=datum.replace(":<", "<");
+}*/
+
+}
+
+function UpdateData() {
     document.querySelectorAll(".data.compact").forEach((el, i) => {
         el.innerHTML = data[i];
-    });*/
+    });
+}
+
+function Start() {
+    UpdateData()
+    setInterval(UpdateData, 1000/vitesse);
 }
 
 function ResetHistory() {/*cet fonction sert a effacer des graphes et changer la vitesse de mise a jour*/

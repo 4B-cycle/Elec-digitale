1bdc5a94b58cf5c700c7010fb6b782db2211b0ec
Dmytro Svitkin <77505900+Dmytro-Svitkin@users.noreply.github.com>
2026-03-15 01:23:53 +0100

    corrige un taille de graphe + changes mineur

--- script.js
+++ script.js
@@ -1,4 +1,47 @@
 /*alert("test de connectivite js a html");✅*/
-function ResetHistory() {
-    alert("Reset Histoire");
+let data=["{l:}<span class=empty>:::::::::::::::::::::::::</span>",
+          "{l:}<span class=empty>:::::::::::::::::::::::::</span>",
+          "{l:}<span class=empty>:::::::::::::::::::::::::</span>",
+          "{l:}<span class=empty>:::::::::::::::::::::::::</span>"];
+
+function Start() {
+    /*
+    document.querySelectorAll(".data.compact").forEach((el, i) => {
+        el.innerHTML = data[i];
+    });*/
+}
+
+function ResetHistory() {/*cet fonction sert a effacer des graphes et changer la vitesse de mise a jour*/
+    data=["{l:}<span class=empty>:::::::::::::::::::::::::</span>",
+          "{l:}<span class=empty>:::::::::::::::::::::::::</span>",
+          "{l:}<span class=empty>:::::::::::::::::::::::::</span>",
+          "{l:}<span class=empty>:::::::::::::::::::::::::</span>"];
+    document.querySelectorAll(".data.compact").forEach((el, i) => {
+        el.innerHTML = data[i];
+    });
+    vitesse=parseFloat(prompt("Changements par seconde: ").replace(",","."));
+    if (isNaN(vitesse)) {
+        vitesse=1;
+    }
+    if (vitesse<0.1&&vitesse>0) {
+        vitesse=0.1;
+    }
+    if (vitesse<0) {
+        vitesse=1;
+    }
+    if (vitesse>100) {
+        vitesse=100;
+    }
+}
+
+function AffDB() {
+    alert("test")
+}
+
+function AffPCB() {
+    alert("test")
+}
+
+function TestLEDs() {
+    alert("test")
 }
